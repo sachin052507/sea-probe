2be9074a028e9a026fca76b0d12e37f00bc2a3e1
Sachin Kadam <sachin_kadam@epam.com>
2025-05-03 19:18:55 +0530

    SeaProbeApplicationTests added

---
+++ src/test/java/com/mavric/assessment/seaprobe/SeaProbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavric.assessment.seaprobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaProbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
