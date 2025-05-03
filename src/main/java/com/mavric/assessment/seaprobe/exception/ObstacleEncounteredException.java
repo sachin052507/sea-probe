eb9166a06a74c6ebe03f0acdd22e8d54244bbd33
Sachin Kadam <sachin_kadam@epam.com>
2025-05-03 19:14:15 +0530

    Exception classes added.

---
+++ src/main/java/com/mavric/assessment/seaprobe/exception/ObstacleEncounteredException.java
@@ -0,0 +1,7 @@
+package com.mavric.assessment.seaprobe.exception;
+
+public class ObstacleEncounteredException extends RuntimeException {
+    public ObstacleEncounteredException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/mavric/assessment/seaprobe/exception/OutOfBoundsException.java
@@ -0,0 +1,7 @@
+package com.mavric.assessment.seaprobe.exception;
+
+public class OutOfBoundsException extends RuntimeException {
+    public OutOfBoundsException(String message) {
+        super(message);
+    }
+}
