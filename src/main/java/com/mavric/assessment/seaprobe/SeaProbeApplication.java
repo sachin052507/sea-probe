915418080d7c6e987334cd6592049629242729e9
Sachin Kadam <sachin_kadam@epam.com>
2025-05-03 19:16:13 +0530

    SeaProbeApplication class added.

---
+++ src/main/java/com/mavric/assessment/seaprobe/SeaProbeApplication.java
@@ -0,0 +1,13 @@
+package com.mavric.assessment.seaprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaProbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaProbeApplication.class, args);
+	}
+
+}
